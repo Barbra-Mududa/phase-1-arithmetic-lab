433612035326b3c8a6a160f3b358c27ded3c3d02
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-16 20:38:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+
+console.log(multiply)
+
+let num3 = 44
+let num4 = 10
+let mod = num3%num4
+
+console.log(mod)
+
+let random = Math.floor(Math.random()*10)+1
+console.log(random)
+
+let max = Math.max(20,18,4,19)
+console.log(max)
